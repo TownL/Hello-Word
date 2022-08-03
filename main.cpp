a690f7cbe68790f3e574449ff8ae35ca4e95ba50
Jan Rodriguez <101287105+TownL@users.noreply.github.com>
2022-08-02 21:19:51 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Jan M. Rodriguez Cumba
+//Este codigo saluda al Mundo de la mejor manera posible con C++ y Xcode.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout << "Soy tu primer programa de C++.\n ";
+    
+    cout << "Hello Word!\n ";
+    
+    cout << "Parece que vas entendiendo como funciona todo...\n ";
+    
+    cout << "eso me gusta y mucho.\n ";
+    
+    return 0;
+    
+}
